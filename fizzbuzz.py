18314723762eb9ea3156674ae1b49f1ea08dd503
hojisu <njisu0610@gmail.com>
2019-06-05 16:57:56 +0900

    feat : fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,13 @@
 n =int(input("input number: "))
-for i in range(1, n+1):
-    if i % 15 == 0:
-        print("fizzbuzz")
-    elif  i % 5 == 0:
-        print("buzz")
-    elif i % 3 == 0:
-        print("fizz")
-    else:
-        print(i)
+
+print([i for i in range(1, n+1)])
+
+#for i in range(1, n+1):
+#    if i % 15 == 0:
+#        print("fizzbuzz")
+#    elif  i % 5 == 0:
+#        print("buzz")
+#    elif i % 3 == 0:
+#        print("fizz")
+#    else:
+#        print(i)

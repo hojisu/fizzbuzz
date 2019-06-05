5b4fe393ed44e42fc9939332285778e7f097cabc
hojisu <njisu0610@gmail.com>
2019-06-05 16:37:29 +0900

    feat : real algorithm for fizzbuzz
    This commit is real fizzbuzz
    I will remake this fizzbuzz with list comprehension.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,10 @@
-for i in range(5):
-    print("Python works!")
+n =int(input("input number: "))
+for i in range(1, n+1):
+    if i % 15 == 0:
+        print("fizzbuzz")
+    elif  i % 5 == 0:
+        print("buzz")
+    elif i % 3 == 0:
+        print("fizz")
+    else:
+        print(i)

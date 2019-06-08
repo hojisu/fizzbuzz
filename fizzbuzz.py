afbe50dc0bb73ae512e7dc3a6a78fdce94529505
hojisu <njisu0610@gmail.com>
2019-06-08 19:07:42 +0900

    feat : listcomprehension fizzbuzz
    listcomprehension fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,4 @@
 n =int(input("input number: "))
 
-print([i for i in range(1, n+1)])
+print(["fizzbuzz" if ( i % 3 == 0 and i % 5 == 0) else "fizz" if i % 3 == 0 else "buzz" if % 5 == 0 else  i for i in range(1, n+1)] )
 
-#for i in range(1, n+1):
-#    if i % 15 == 0:
-#        print("fizzbuzz")
-#    elif  i % 5 == 0:
-#        print("buzz")
-#    elif i % 3 == 0:
-#        print("fizz")
-#    else:
-#        print(i)

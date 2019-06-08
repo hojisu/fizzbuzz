1f7d6e3632d0782f16069ba2312954743a95653d
hojisu <njisu0610@gmail.com>
2019-06-08 19:13:33 +0900

    feat : listcomprehension fizzbuzz
    An error code in the previous post "listcomprehension fizzbuzz" has been corrected.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,4 @@
 n =int(input("input number: "))
 
-print(["fizzbuzz" if ( i % 3 == 0 and i % 5 == 0) else "fizz" if i % 3 == 0 else "buzz" if % 5 == 0 else  i for i in range(1, n+1)] )
+print(["fizzbuzz" if ( i % 3 == 0 and i % 5 == 0) else "fizz" if i % 3 == 0 else "buzz" if i  % 5 == 0 else  i for i in range(1, n+1)] )
 
